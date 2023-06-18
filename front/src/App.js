8552ba5cef469b81fd508dd8bed5c0a07c504fdb
namnahyun <namnahyundev@gmail.com>
2023-06-18 17:22:49 +0900

    폴더 구조 변경

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './page/Main';
+import Login from './page/Login';
+import SignUp from './page/SignUp';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Main />}></Route>
+					<Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
